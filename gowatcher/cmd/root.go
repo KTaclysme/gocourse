b75072f90900d249ef7075c93fc3b4a98954431f
kevin.ta <kevin.ta@emeria.eu>
2025-05-27 14:15:05 +0200

    cmd

---
+++ gowatcher/cmd/root.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "gowatcher",
+	Short: "Gowatcher est un outil pour vérifier l'accessibilité des URLs.",
+	Long:  "Un outil CLI en Go pour vérifier l'état d'URLs, gérer la concurrence et exporter les résultats.",
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Erreur: %v\n", err)
+		os.Exit(1)
+	}
+}
--- gowatcher/main.go
+++ gowatcher/main.go
@@ -1,57 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"sync"
-
-	"github.com/KTaclysme/gowatcher/checker"
+	"github.com/KTaclysme/gowatcher/cmd"
 )
 
 func main() {
-	targets := []string{
-		"https://www.google.com",
-		"https://www.notarealwebsite.abc",
-		"https://www.github.com",
-		"https://www.movie.database/film/details",
-		"https://www.gaming.news/release/new-game",
-		"https://www.health.clinic/appointment/online",
-		"https://www.car.manufacturer/model/electric",
-		"https://www.home.decor/ideas/living-room",
-		"https://www.environmental.org/project/clean-water",
-		"https://www.space.agency/mission/mars",
-		"https://www.fashion.magazine/trend/summer",
-		"https://www.tech.conference/schedule/day1",
-		"https://www.food.blog/recipe/dessert",
-		"https://www.online.course/programming/python",
-		"https://www.travel.guide/city/paris",
-		"https://www.music.label/artist/new-album",
-		"https://www.sports.club/events/match",
-		"https://www.photography.tips/technique/lighting",
-		"https://www.diy.tools/review/drill",
-		"https://www.pet.vet/service/vaccination",
-		"https://www.gardening.store/seeds/flower",
-		"https://www.finance.advice/retirement/planning",
-		"https://www.history.podcast/episode/ww2",
-		"https://www.language.exchange/partner/find",
-		"https://www.book.review/author/classic",
-		"https://www.movie.review/genre/comedy",
-		"https://www.gaming.forum/topic/strategy",
-	}
-
-	var wg sync.WaitGroup
-
-	wg.Add(len(targets))
-	for _, url := range targets {
-		go func(u string) {
-			result := checker.CheckURL(u)
-			defer wg.Done()
-			if result.Err != nil {
-				fmt.Printf("KO %s: erreur: %v\n", result.Target, result.Err)
-			} else {
-				fmt.Printf("OK %s - Status : %s", result.Target, result.Status)
-			}
-		}(url)
-	}
-
-	wg.Wait()
+	cmd.Execute()
 }
